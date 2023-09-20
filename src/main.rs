7eeed325dce9b38c5ca26b9b296310550fc00c39
Noor Mohammed <noormohammedb@protonmail.com>
2023-09-20 11:46:33 +0400

    libp2p ping

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use libp2p::swarm::{keep_alive, NetworkBehaviour, SwarmBuilder};
-use libp2p::{identity, ping, PeerId};
+use futures::StreamExt;
+use libp2p::swarm::{keep_alive, NetworkBehaviour, SwarmBuilder, SwarmEvent};
+use libp2p::{identity, ping, Multiaddr, PeerId};
 use std::error::Error;
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-  println!("Hello, world!");
+  println!("libp2p ping sample");
 
   let local_key = identity::Keypair::generate_ed25519();
   let local_peer_id = PeerId::from(local_key.public());
@@ -16,7 +17,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
   let mut swarm = SwarmBuilder::with_async_std_executor(transport, behavior, local_peer_id).build();
 
-  Ok(())
+  swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+  if let Some(addr) = std::env::args().nth(1) {
+    let remote: Multiaddr = addr.parse()?;
+    swarm.dial(remote)?;
+    println!("Dialed {addr}");
+  }
+
+  loop {
+    match swarm.select_next_some().await {
+      SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
+      SwarmEvent::Behaviour(event) => println!("{event:?}"),
+      _ => {}
+    }
+  }
 }
 
 #[derive(NetworkBehaviour, Default)]
